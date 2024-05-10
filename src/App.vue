5ba12283b472a1ca4ad620467d90b7e4788c34ac
IngDavidTM <david-1011@hotmail.com>
2024-05-10 14:57:50 -0500

    Add slots props

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,6 +20,7 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 
 export default {
@@ -21,6 +28,7 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals
   },
   data() {
     return {
