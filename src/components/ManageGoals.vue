bf07c4e189174ab3890aa4e6543d9cde0860351c
IngDavidTM <david-1011@hotmail.com>
2024-05-11 17:47:21 -0500

    Use teleport for semanthics

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,16 +3,18 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsValid">
-      <h2>Input is Invalid!</h2>
-      <p>Please enter at least few characters...</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+    <teleport to="body">
+      <error-alert v-if="inputIsValid">
+        <h2>Input is Invalid!</h2>
+        <p>Please enter at least few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script>
-import ErrorAlert from './ErrorAlert.vue';
+import ErrorAlert from "./ErrorAlert.vue";
 
 export default {
   components: {
@@ -26,13 +28,13 @@ export default {
   methods: {
     setGoal() {
       const enteredValue = this.$refs.goal.value;
-      if (enteredValue === '') {
+      if (enteredValue === "") {
         this.inputIsValid = true;
       }
     },
     confirmError() {
       this.inputIsValid = false;
-    }
-  }
+    },
+  },
 };
 </script>
