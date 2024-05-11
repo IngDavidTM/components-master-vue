afecc2f61fcd25e69443e89173039a3369285a2c
IngDavidTM <david-1011@hotmail.com>
2024-05-10 22:48:42 -0500

    Add dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
+    <button
+      @click="setSelectedComponent('active-goals')"
+    >Active Goals</button>
+    <button
+      @click="setSelectedComponent('manage-goals')"
+    >Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
+    <!-- <badge-list></badge-list>
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
@@ -12,32 +21,42 @@
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps.anotherProp }}</p>
       </template>
-    </course-goals>
+    </course-goals> -->
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
 
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    ActiveGoals,
+    ManageGoals
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
+  },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
   },
 };
 </script>
